087ce16d86e178218bdea0a5a18cb8872d18cb63
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-01-27 15:18:09 +0300

    minor

--- resources/views/livewire/pages/profile/update-password-form.blade.php
+++ resources/views/livewire/pages/profile/update-password-form.blade.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Volt\Component;
+use MikeMcLin\WpPassword\Facades\WpPassword;
 
-new class extends Component
-{
+new class extends Component {
     public string $current_password = '';
     public string $password = '';
     public string $password_confirmation = '';
@@ -17,15 +18,28 @@ new class extends Component
      */
     public function updatePassword(): void
     {
-        try {
-            $validated = $this->validate([
-                'current_password' => ['required', 'string', 'current_password'],
-                'password' => ['required', 'string', Password::defaults(), 'confirmed'],
-            ]);
-        } catch (ValidationException $e) {
-            $this->reset('current_password', 'password', 'password_confirmation');
 
-            throw $e;
+
+        if (Auth::user()['wp_password']) {
+            try {
+                $validated = $this->validate([
+                    'current_password' => ['required', 'string'],
+                    'password' => ['required', 'string', Password::defaults(), 'confirmed'],
+                ]);
+            } catch (ValidationException $e) {
+                $this->reset('current_password', 'password', 'password_confirmation');
+                throw $e;
+            }
+        } else {
+            try {
+                $validated = $this->validate([
+                    'current_password' => ['required', 'string', 'current_password'],
+                    'password' => ['required', 'string', Password::defaults(), 'confirmed'],
+                ]);
+            } catch (ValidationException $e) {
+                $this->reset('current_password', 'password', 'password_confirmation');
+                throw $e;
+            }
         }
 
         Auth::user()->update([
@@ -35,29 +49,35 @@ new class extends Component
         $this->reset('current_password', 'password', 'password_confirmation');
 
         $this->dispatch('password-updated');
+
+
     }
 }; ?>
 
 <section>
-    <h2 >{{ __('Пароль') }}</h2>
+    <h2>{{ __('Пароль') }}</h2>
 
     <form wire:submit="updatePassword" class="mt-6 space-y-6">
         <div>
-            <x-input-label for="update_password_current_password" :value="__('Текущий пароль')" />
-            <x-text-input wire:model="current_password" id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
+            <x-input-label for="update_password_current_password" :value="__('Текущий пароль')"/>
+            <x-text-input wire:model="current_password" id="update_password_current_password" name="current_password"
+                          type="password" class="mt-1 block w-full" autocomplete="current-password"/>
+            <x-input-error :messages="$errors->get('current_password')" class="mt-2"/>
         </div>
 
         <div>
-            <x-input-label for="update_password_password" :value="__('Новый пароль')" />
-            <x-text-input wire:model="password" id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-label for="update_password_password" :value="__('Новый пароль')"/>
+            <x-text-input wire:model="password" id="update_password_password" name="password" type="password"
+                          class="mt-1 block w-full" autocomplete="new-password"/>
+            <x-input-error :messages="$errors->get('password')" class="mt-2"/>
         </div>
 
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Подтвердите пароль')" />
-            <x-text-input wire:model="password_confirmation" id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-input-label for="update_password_password_confirmation" :value="__('Подтвердите пароль')"/>
+            <x-text-input wire:model="password_confirmation" id="update_password_password_confirmation"
+                          name="password_confirmation" type="password" class="mt-1 block w-full"
+                          autocomplete="new-password"/>
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2"/>
         </div>
 
         <div class="flex items-center gap-4">
