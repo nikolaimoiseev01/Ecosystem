9c3d92793961306d3473894afda854d5887d7d7f
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-03-24 14:26:17 +0300

    Minor

--- resources/views/livewire/pages/account/lessons-page.blade.php
+++ resources/views/livewire/pages/account/lessons-page.blade.php
@@ -4,8 +4,8 @@
     {{--        <p>Пройдено тестов: {{$user->testResult->count()}}; Всего--}}
     {{--            балов: {{$user->testResult->sum('applicant_points')}}--}}
     {{--            из {{$user->testResult->sum('questions_number')}}</p>--}}
-    {{--    </div>--}}
-    <x-button wire:click="downloadDiploma()" class="mb-8 text-lg">Скачать сертификат</x-button>
+{{--        </div>--}}
+{{--    <x-button wire:click="downloadDiploma()" class="mb-8 text-lg">Скачать сертификат</x-button>--}}
     <div class="flex flex-col gap-8 mb-32">
         @foreach($lessons as $lesson)
             <div class="flex flex-col p-8 bg-gray-300 rounded-xl">
