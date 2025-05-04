0e71e9bca85a40a45d44b2d4aa269e90dd8b0f02
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-05-04 23:43:45 +0300

    Make Media Club

--- resources/views/livewire/components/footer.blade.php
+++ resources/views/livewire/components/footer.blade.php
@@ -1,6 +1,7 @@
-<footer class="bg-green-500 text-white mt-auto rounded-tl-[200px] rounded-tr-[200px] md:rounded-none flex flex-col gap-8 items-center text-white py-16">
+<footer x-data="{ isMediaClub: window.location.pathname === '/media-club' }"
+    class="bg-green-500 text-white mt-auto rounded-tl-[200px] rounded-tr-[200px] md:rounded-none flex flex-col gap-8 items-center text-white py-16">
     <h1 class="">КОНТАКТЫ</h1>
-    <h1 class="">+79932875359</h1>
+    <h1 x-show="!isMediaClub" class="">+79932875359</h1>
     <h2 class="text-white">cu_ecosistema@mail.ru</h2>
     <p class="text-sm text-center">Организовано Всероссийским общественным экологическим движением «Экосистема»<br>
         при поддержке Госкорпорации «Росатом»</p>
