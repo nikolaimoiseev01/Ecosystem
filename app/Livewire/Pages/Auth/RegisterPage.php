b50f7c6ab8147993555d0208cf533a78a859b32d
Nikolai <nikolaimoiseev01@gmail.com>
2026-04-11 10:14:52 +0300

    V 2.0

--- app/Livewire/Pages/Auth/RegisterPage.php
+++ app/Livewire/Pages/Auth/RegisterPage.php
@@ -206,6 +206,7 @@ class RegisterPage extends Component
 
             // Send message.
             $response = $oSMSAero->send($phone_number, $message, $type);
+            dd(config('app.debug'), $response);
         }
 
         // Default response data -> json. However we can get response in XML format.
